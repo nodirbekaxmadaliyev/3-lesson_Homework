60b0fc93e2a2002c5992736078a7805da7aad0b5
Nodirbek Axmadaliyev <nodirbekaxmadaliyev4888@gmail.com>
2024-04-06 22:28:47 +0500

    updates

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Ikki sonning yig'indisini xisoblash dasturi:\n");
+Console.WriteLine("Birinchi sonni kiriting : ");
+long firstInt = Convert.ToInt64(Console.ReadLine());
+Console.WriteLine("Ikkinchi sonni kiriting : ");
+int secondInt = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\nIkki sonning yig'indisi " + (firstInt + secondInt) + " ga teng.\n\n");
